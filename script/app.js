5f6cb0850aca01af8f9978f97961b0a5cf165fdd
MLoth <MLoth@users.noreply.github.com>
2019-11-25 08:33:16 +0100

    Graph update

--- script/app.js
+++ script/app.js
@@ -1,48 +1,54 @@
 function handleDropdownButton() {
-	var show = 'is-open';
-	var elements = document.getElementsByClassName('js-dropdown-toggle');
+	let show = 'is-open';
+	let elements = document.getElementsByClassName('js-dropdown-toggle');
 
 	//Hide/Show Dropdown Menu on Click
-	for (var i = 0; i < elements.length; i++) {
-		elements[i].addEventListener("click", function() {
-			removeClass(elements,show);
-			if(hasClass(this,show)) {
-				removeClass(this,show);
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].addEventListener('click', function() {
+			removeClass(elements, show);
+			if (hasClass(this, show)) {
+				removeClass(this, show);
 			} else {
-				addClass(this,show);
+				addClass(this, show);
 			}
 		});
 	}
 
 	//Hide Dropdown when clicked outside of the Menu
-	document.addEventListener('click', function (event) {
-		for (var i = 0; i < elements.length; i++) {
-			if (event.target.closest('.js-dropdown-toggle')) return;
-			removeClass(elements[i],show);
+	document.addEventListener('click', function(event) {
+		for (let i = 0; i < elements.length; i++) {
+			if (event.target.closest('.js-dropdown-toggle')) return; // closest; van de selector tot aan de root zoeken naar deze selector-string
+			removeClass(elements[i], show);
 		}
-	}, false);
+	});
 
 	function addClass(elements, myClass) {
-		if (!elements) { return; }
-		if (typeof(elements) === 'string') {
-			elements = document.querySelectorAll(elements);
+		if (!elements) {
+			return;
 		}
-		else if (elements.tagName) { elements=[elements]; }
-		for (var i=0; i<elements.length; i++) {
-			if ( (' '+elements[i].className+' ').indexOf(' '+myClass+' ') < 0 ) {
+		if (typeof elements === 'string') {
+			elements = document.querySelectorAll(elements);
+		} else if (elements.tagName) {
+			elements = [elements];
+		}
+		for (let i = 0; i < elements.length; i++) {
+			if ((' ' + elements[i].className + ' ').indexOf(' ' + myClass + ' ') < 0) {
 				elements[i].className += ' ' + myClass;
 			}
 		}
 	}
 	function removeClass(elements, myClass) {
-		if (!elements) { return; }
-		if (typeof(elements) === 'string') {
-			elements = document.querySelectorAll(elements);
+		if (!elements) {
+			return;
 		}
-		else if (elements.tagName) { elements=[elements]; }
-		var reg = new RegExp('(^| )'+myClass+'($| )','g');
-		for (var i=0; i<elements.length; i++) {
-			elements[i].className = elements[i].className.replace(reg,' ');
+		if (typeof elements === 'string') {
+			elements = document.querySelectorAll(elements);
+		} else if (elements.tagName) {
+			elements = [elements];
+		}
+		let reg = new RegExp('(^| )' + myClass + '($| )', 'g');
+		for (let i = 0; i < elements.length; i++) {
+			elements[i].className = elements[i].className.replace(reg, ' ');
 		}
 	}
 
@@ -51,7 +57,7 @@ function handleDropdownButton() {
 	}
 }
 
-document.addEventListener( 'DOMContentLoaded', function() {
-	console.log( 'Script loaded!' );
+document.addEventListener('DOMContentLoaded', function() {
+	console.log('Script loaded!');
 	handleDropdownButton();
 });
--- script/dashboard-chart.js
+++ script/dashboard-chart.js
@@ -1,7 +1,6 @@
-let daySelect,
-	graph;
+let daySelect, graph;
 
-const drawChart = ( data ) => {
+const drawChart = data => {
 	let ctx = graph.getContext('2d');
 
 	// let chart = new Chart(ctx, {
@@ -9,26 +8,30 @@ const drawChart = ( data ) => {
 		type: 'line',
 		data: {
 			labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7'],
-			datasets: [{
-				label: 'Visitors',
-				data: data,
-				borderColor: '#A3A0FB',
-				backgroundColor: '#A3A0FB10',
-				pointBackgroundColor: 'white',
-				lineTension: .3,
-				borderWidth: 2,
-				pointRadius: 4,
-			}],
+			datasets: [
+				{
+					label: 'Visitors',
+					data: data,
+					borderColor: '#A3A0FB',
+					backgroundColor: '#A3A0FB10',
+					pointBackgroundColor: 'white',
+					lineTension: 0.3,
+					borderWidth: 2,
+					pointRadius: 4
+				}
+			]
 		},
 		options: {
-			defaultFontColor: Chart.defaults.global.defaultFontColor = '#808495',
+			defaultFontColor: (Chart.defaults.global.defaultFontColor = '#808495'),
 			scales: {
-				yAxes: [{
-					ticks: {
-						min: 0,
-						max: 50,
+				yAxes: [
+					{
+						ticks: {
+							min: 0,
+							max: 50
+						}
 					}
-				}]
+				]
 			},
 			tooltips: {
 				xPadding: 10,
@@ -38,52 +41,53 @@ const drawChart = ( data ) => {
 			legend: {
 				position: 'bottom',
 				labels: {
-					defaultFontFamily: Chart.defaults.global.defaultFontFamily = "'Source Sans Pro', 'Helvetica', 'arial', 'sans-serif'",
+					defaultFontFamily: (Chart.defaults.global.defaultFontFamily =
+						"'Source Sans Pro', 'Helvetica', 'arial', 'sans-serif'"),
 					boxWidth: 15
 				}
 			},
-			responsive: true,
+			responsive: true
 		}
 	});
 	// document.querySelector('.js-chartjsLegend').innerHTML = chart.generateLegend();
-}
+};
 
-const getData = ( json ) => {
+const getData = json => {
 	let data = [];
 	// let data = [],
 	// 	labels = [];
+	console.log(json);
 
-	json.map( day => {
+	json.map(day => {
 		// labels.push( day.tijdstip );
-		data.push( day.aantalBezoekers );
+		data.push(day.AantalBezoekers);
 	});
 
 	// drawChart( data, labels );
-	drawChart( data );
-}
+	drawChart(data);
+};
 
-const getVisitorsByDay = ( day ) => {
-	let endpoint = `https://iotcloud-nmct.azurewebsites.net/api/visitors/${ day }`;
+const getVisitorsByDay = day => {
+	let endpoint = `https://iotcloud-nmct.azurewebsites.net/api/visitors/${day}`;
 
-	fetch( endpoint )
-		.then( r => r.json() )
-		.then( json => {
-			getData( json );
+	fetch(endpoint)
+		.then(r => r.json())
+		.then(json => {
+			getData(json);
 		})
-		.catch( e => console.error( e ) )
-}
+		.catch(e => console.error(e));
+};
 
 const init = () => {
-	daySelect = document.querySelector( '.js-day-select' ),
-		graph = document.querySelector( '.js-graph' );
+	(daySelect = document.querySelector('.js-day-select')), (graph = document.querySelector('.js-graph'));
 
-	daySelect.addEventListener( 'change', function( e ) {
-		getVisitorsByDay( e.target.value );
+	daySelect.addEventListener('change', function(e) {
+		getVisitorsByDay(e.target.value);
 	});
 
-	getVisitorsByDay( 'maandag' );
-}
+	getVisitorsByDay('maandag');
+};
 
-document.addEventListener( 'DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 	init();
 });
